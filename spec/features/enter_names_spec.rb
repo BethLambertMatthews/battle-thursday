7951958d41da5d43017224849a6d02292212af98
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-10-07 14:53:13 +0100

    Continuing entering player names

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,9 @@ feature 'Testing names: ' do
     fill_in :player_1_name, with: 'Beth'
     fill_in :player_2_name, with: 'Michelle'
     click_button 'Submit'
+
+    save_and_open_page
+
     expect(page).to have_content 'Beth vs Michelle'
   end
 end
